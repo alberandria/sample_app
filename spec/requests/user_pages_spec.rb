dab8dfd07c25af610335be8287fa8153f19c39ca
alberandria <alberandria@gmail.com>
2013-03-18 20:25:29 -0300

    Fixed tests

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,10 +4,16 @@ describe "User pages" do
 
   subject { page }
 
-  describe "signup page" do
-    before { visit signup_path }
+  shared_examples_for "all user pages" do
+    it { should have_selector('h1',    text: heading) }
+    it { should have_selector('title', text: full_title(page_title)) }
+  end
 
-    it { should have_selector('h1',    text: 'Sign up') }
-    it { should have_selector('title', text: full_title('Sign up')) }
+  describe "Sign Up page" do
+    before { visit signup_path }
+    let(:heading)    { 'Sign Up' }
+    let(:page_title) { 'Sign Up' }
+
+    it_should_behave_like "all user pages"
   end
 end
